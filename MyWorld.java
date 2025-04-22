7090ecb538b07494378ce301a88dec9b1601c02f
Johnning11 <350797049@gapps.yrdsb.ca>
2025-04-22 14:17:11 -0400

    Hero Move

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        Hero hero = new Hero();
+        hero hero = new hero();
         addObject(hero, 100, 100);
         
         Bomb bomb = new Bomb();
