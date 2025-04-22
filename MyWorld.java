fe997245a6851b40a6741fbeae40fffa2af42c16
Johnning11 <350797049@gapps.yrdsb.ca>
2025-04-22 14:10:20 -0400

    Add Hero

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-
-
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
         
         Bomb bomb = new Bomb();
         addObject(bomb, 600, 100);
