e15b1c46e2dc1437d3042f4c615c6399940548e4
caydench <349581918@gapps.yrdsb.ca>
2025-04-22 14:21:45 -0400

    Reset Bomb

--- Bomb.java
+++ Bomb.java
@@ -11,5 +11,18 @@ public class Bomb extends Actor
     public void act()
     {
         move(-5);
+        
+        if(getX() <= 0) {
+            resetBomb();
+        }
+    }
+    
+    public void resetBomb() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+        setLocation(600, 100);
+        } else {
+        setLocation(600, 300);
+        }
     }
 }
